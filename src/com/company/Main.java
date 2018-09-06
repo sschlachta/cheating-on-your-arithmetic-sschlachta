61b22e0fd4768f63c07064b51eb4cbf99e0c26de
sschlachta <ss772@pvsd.org>
2018-09-05 21:37:58 -0400

    This be it.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+      class Tester {
+
+          int solOne = 79 + 3 * (4 + 82 - 68) - 7 + 19;
+          int solTwo = (179 + 21 + 10) / 7 + 181;
+          int solThree = 10389 * 56 * 11 + 2246;
+
+          String probOne = "79 + 3 * (4 + 82 - 68) - 7 + 19 = " + solOne;
+          String probTwo = "(179 + 21 + 10) / 7 + 181 = " + solTwo;
+          String probThree = "10389 * 56 * 11 + 2246 = " + solThree;
+
+          System.out.println(probOne);
+          System.out.println(probTwo);
+          System.out.println(probThree);
+
+      }
+
     }
 }
